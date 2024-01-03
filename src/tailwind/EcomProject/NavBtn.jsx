4d0b2a23542013301e470d4dfcf780d4db429254
BoopathiN678 <153748177+BoopathiN678@users.noreply.github.com>
2024-01-03 16:45:25 +0530

    Update NavBtn.jsx

--- src/tailwind/EcomProject/NavBtn.jsx
+++ src/tailwind/EcomProject/NavBtn.jsx
@@ -1,7 +1,7 @@
 import { MdKeyboardArrowDown } from "react-icons/md";
 export const NavBtn=()=>{
     return(
-        <div className="bg-[white] hidden sm:flex p-2 gap-6 justify-evenly ">
+        <div className="bg-[white] flex p-2 gap-6 sm:justify-evenly overflow-x-auto">
           
                 <button className="font-sans font-bold text-[15px] hover:text-[#6464fb] flex gap-1">Electronics<span className="   text-[20px]  hover:rotate-180 hover:[blue]"><MdKeyboardArrowDown /></span></button>
                 <button className="font-sans font-bold text-[15px] hover:text-[#6464fb] flex gap-1 ">TV&Appliances<span className="   text-[20px]  hover:rotate-180 hover:[blue]"><MdKeyboardArrowDown /></span></button>
@@ -11,7 +11,7 @@ export const NavBtn=()=>{
                 <button className="font-sans font-bold text-[15px] hover:text-[#6464fb] flex gap-1">Home&Furniture<span className="   text-[20px]  hover:rotate-180 hover:[blue]"><MdKeyboardArrowDown /></span></button>
                 <button className="font-sans font-bold text-[15px] hover:text-[#6464fb] flex gap-1">Sports,Books&More<span className="   text-[20px]  hover:rotate-180 hover:[blue]"><MdKeyboardArrowDown /></span></button>
                 <button className="font-sans font-bold text-[15px] hover:text-[#6464fb] flex gap-1">Flights<span className="   text-[20px]  hover:rotate-180 hover:[blue]"><MdKeyboardArrowDown /></span></button>
-                <button className="font-sans font-bold text-[15px] hover:text-[#6464fb] flex gap-1">Offer Zone<span className="  text-[20px]  hover:rotate-180 hover:[blue]"><MdKeyboardArrowDown /></span></button>
+                <button className="font-sans font-bold text-[15px] hover:text-[#6464fb] w-[200px] flex gap-1">OffersZone<span className="  text-[20px]  hover:rotate-180 hover:[blue]"><MdKeyboardArrowDown /></span></button>
             
         </div>
     )
